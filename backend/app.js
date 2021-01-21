6415a2ad8b4c1df224a934a7b03c10d97922a8b2
powelljep <powell.john2011@gmail.com>
2021-01-21 16:31:11 +0300

    adding database connection

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 const app = express();
 
-module.exports();
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pascalflixdb'
+  })
+  connection.connect();
+  connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log('The solution is: ', rows[0].solution)
+  })
+  
+  connection.end()
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     next();
+//   });
+// app.use(bodyParser.json());+
+
+
+
+
+// module.exports=app;
